31231676a320dab8fe94fdd5a39a3ea42786815e
Brenon <eibrenon88@gmail.com>
2023-04-18 19:24:35 +0000

    add ex022

---
+++ ex022.py
@@ -0,0 +1,8 @@
+nome = str(input(print('Digite seu nome completo: '))).strip()
+print('Analisando o seu nome...')
+print('O seu nome em maiusculas fica {}'.format(nome.upper()))
+print('O seu nome em minusculas fica {}'.format(nome.lower()))
+print('O seu nome tem ao todo {} letras'.format(len(nome) - nome.count(' ')))
+# print('O seu primeiro nome tem {} letras'.format(nome.find(' ')))
+separa = nome.split()
+print('Seu primeiro nome é {} e ele tem {} letras'.format(separa[0], len(separa[0])))
