0f692fc4a0b772997eda70f0a9023eb21779ecff
Brenon <eibrenon88@gmail.com>
2023-04-23 03:27:54 +0000

    Atualizando os ex 24 e 25

--- ex024.py
+++ ex024.py
@@ -1,2 +1,2 @@
 cid = str(input('Em que cidade você nasceu? ')).strip()
-print(cid[:5].upper() == 'SANTO')
+print('Na cidade que vc nasceu tem santo? {}'.format(cid[:5].upper() == 'SANTO'))
--- ex025.py
+++ ex025.py
@@ -1,2 +1,2 @@
 nome = str(input('Qual é o seu nome? ')).split()
-print('Seu nome tem Silva? {}'.format('silva' in nome.lower()))
+print('Seu nome tem Silva? {}'.format(nome))
