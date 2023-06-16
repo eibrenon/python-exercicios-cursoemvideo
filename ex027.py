4abe8a8dc165b6f8f70c4a6404ca6add4b07f902
José Brenon <eibrenon88@gmail.com>
2023-06-16 17:11:04 -0300

    Adicionando grande a frase

--- ex027.py
+++ ex027.py
@@ -2,6 +2,6 @@
 # mostrando em seguida o primeiro e o último nome separadamente.
 n = str(input('Digite seu nome completo: ')).strip()
 nome = n.split()
-print('Olá, é um prazer te conhecer!')
+print('Olá, é um grande prazer te conhecer!')
 print('Seu primeiro nome é {}'.format(nome[0]))
 print('Seu último nome é {}'.format(nome[len(nome)-1]))
