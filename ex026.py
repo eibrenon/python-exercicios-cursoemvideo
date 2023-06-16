aea70ff689fcc010d7027c74997225387bed590a
José Brenon <eibrenon88@gmail.com>
2023-06-16 17:08:42 -0300

    Trocando o a pelo A

--- ex026.py
+++ ex026.py
@@ -3,6 +3,6 @@
 # Em que posição ela aparece a primeira vez.
 # Em que posição ela apareçe a última vez.
 frase = str(input('Digite uma frase: ')).upper().strip()
-print('A letra a aparece {} na frase'.format(frase.count('A')))
+print('A letra A aparece {} na frase'.format(frase.count('A')))
 print('A primeira letra A apareceu na posição {}'.format(frase.find('A') + 1))
 print('A última letra A apareceu na posição {}'.format(frase.rfind('A') + 1))
